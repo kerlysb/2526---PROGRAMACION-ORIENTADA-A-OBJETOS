eb9a229aeeb5b3315a227bee8640212145d5e53b
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-03 22:52:49 -0500

    abstraccion

---
+++ Abstracción/abstracción.py
@@ -0,0 +1,24 @@
+class CuentaBancaria:
+    def __init__(self, titular, saldo_inicial):
+        self.titular = titular
+        self.saldo = saldo_inicial
+
+    # Métodos "públicos" que abstraen el funcionamiento interno
+    def depositar(self, monto):
+        self.saldo += monto
+
+    def retirar(self, monto):
+        if monto <= self.saldo:
+            self.saldo -= monto
+        else:
+            print("Fondos insuficientes")
+
+    def mostrar_saldo(self):
+        print(f"Saldo de {self.titular}: {self.saldo} USD")
+
+
+# Uso
+cuenta = CuentaBancaria("Ana", 100)
+cuenta.depositar(50)
+cuenta.retirar(30)
+cuenta.mostrar_saldo()
