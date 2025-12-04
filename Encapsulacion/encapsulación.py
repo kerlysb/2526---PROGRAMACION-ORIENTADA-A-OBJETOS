672a5c4b70d207d1300bf08fd5edaf5b5add7c83
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-03 23:04:40 -0500

    encapsulacion

---
+++ Encapsulacion/encapsulación.py
@@ -0,0 +1,25 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self._nombre = nombre      # "protegido"
+        self._edad = edad          # "protegido"
+
+    # Getter
+    def get_edad(self):
+        return self._edad
+
+    # Setter con validación
+    def set_edad(self, nueva_edad):
+        if nueva_edad >= 0:
+            self._edad = nueva_edad
+        else:
+            print("La edad no puede ser negativa")
+
+    def mostrar(self):
+        print(f"{self._nombre} tiene {self._edad} años")
+
+
+p = Persona("Luis", 20)
+p.mostrar()
+p.set_edad(25)
+p.mostrar()
+p.set_edad(-5)   # No permite valor negativo
