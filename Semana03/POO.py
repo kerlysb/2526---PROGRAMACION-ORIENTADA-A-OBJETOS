f59d53daa43d91dfffe16006560ec198334dfa83
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-12 14:42:30 -0500

    Muestra temperaturas por día

--- Semana03/POO.py
+++ Semana03/POO.py
@@ -4,3 +4,8 @@ class SemanaClima:
     def __init__(self, temperaturas):
         self.__temperaturas = temperaturas  # Encapsulamiento
         self.dias = ["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"]
+
+        def mostrar_datos_diarios(self):
+            """Muestra temperaturas por día"""
+            for i, dia in enumerate(self.dias):
+                print(f"{dia}: {self.__temperaturas[i]:.1f}°C")
