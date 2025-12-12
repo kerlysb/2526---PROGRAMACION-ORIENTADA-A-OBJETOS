52f6e16b8427f0418bf6ee9327035bc20a11639a
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-12 14:38:55 -0500

    calculo de promedio semanal

--- Semana03/Programación_tradicional.py
+++ Semana03/Programación_tradicional.py
@@ -2,3 +2,6 @@
 def procesar_temperaturas_diarias(temperaturas):
     """Procesa lista de 7 temperaturas diarias"""
     return temperaturas
+def calcular_promedio_semanal(temperaturas):
+    """Calcula promedio de temperaturas semanales"""
+    return sum(temperaturas) / len(temperaturas)
