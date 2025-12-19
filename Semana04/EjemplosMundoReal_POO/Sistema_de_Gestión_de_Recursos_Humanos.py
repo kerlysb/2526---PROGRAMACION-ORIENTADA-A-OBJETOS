1347a67f48bf91282287718b59f0e208f2e39512
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-18 23:29:58 -0500

    calcular salario de empleado

--- Semana04/EjemplosMundoReal_POO/Sistema_de_Gestión_de_Recursos_Humanos.py
+++ Semana04/EjemplosMundoReal_POO/Sistema_de_Gestión_de_Recursos_Humanos.py
@@ -5,3 +5,9 @@ class Empleado:
         self._id_empleado = id_empleado  # Encapsulación
         self._salario_base = 0
 
+    def calcular_salario(self):
+        """Método que se sobrescribe - polimorfismo"""
+        return self._salario_base
+
+    def descripcion(self):
+        return f"Empleado: {self.nombre} (ID: {self._id_empleado})"
