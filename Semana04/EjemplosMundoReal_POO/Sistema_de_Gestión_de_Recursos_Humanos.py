58f91edd3ca058a422bd4620c5644fc6f3168f20
kerly suarez <kerly_suarezb@hotmail.com>
2025-12-18 23:36:40 -0500

    Clase Desarrollador

--- Semana04/EjemplosMundoReal_POO/Sistema_de_Gestión_de_Recursos_Humanos.py
+++ Semana04/EjemplosMundoReal_POO/Sistema_de_Gestión_de_Recursos_Humanos.py
@@ -11,3 +11,15 @@ class Empleado:
 
     def descripcion(self):
         return f"Empleado: {self.nombre} (ID: {self._id_empleado})"
+
+# Clase Desarrollador - herencia simple
+class Desarrollador(Empleado):
+    def __init__(self, nombre, id_empleado, lenguaje):
+        super().__init__(nombre, id_empleado)
+        self.lenguaje = lenguaje
+        self._salario_base = 2500
+
+    def calcular_salario(self):
+        """Polimorfismo: salario con bono por lenguaje"""
+        bono = 500 if self.lenguaje == "Python" else 200
+        return self._salario_base + bono
