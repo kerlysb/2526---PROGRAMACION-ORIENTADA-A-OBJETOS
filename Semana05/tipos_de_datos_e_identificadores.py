720c3a38d1ee1ee1ba01d5c02a7af2f9194c7842
kerly suarez <kerly_suarezb@hotmail.com>
2026-01-08 09:59:51 -0500

    Programa para calcular el área de un rectángulo

---
+++ Semana05/tipos_de_datos_e_identificadores.py
@@ -0,0 +1,15 @@
+"""
+Programa para calcular el área de un rectángulo.
+"""
+
+def calcular_area(largo: float, ancho: float) -> float:
+    """
+    Calcula el área de un rectángulo.
+
+    Args:
+        largo (float): Largo del rectángulo en metros.
+        ancho (float): Ancho del rectángulo en metros.
+
+    Returns:
+        float: Área calculada.
+    """
