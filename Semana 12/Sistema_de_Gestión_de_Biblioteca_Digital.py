a06c5f827c1f1df86e839091c292a08a8064925a
kerly suarez <kerly_suarezb@hotmail.com>
2026-03-02 11:15:49 -0500

    Creación de clase usuario

--- Semana 12/Sistema_de_Gestión_de_Biblioteca_Digital.py
+++ Semana 12/Sistema_de_Gestión_de_Biblioteca_Digital.py
@@ -9,3 +9,11 @@ class Libro:
     def __str__(self) -> str:
         titulo, autor = self._titulo_autor
         return f"{titulo} por {autor} (Categoría: {self.categoria}, ISBN: {self.isbn})"
+class Usuario:
+    def __init__(self, nombre: str, id_usuario: str):
+        self.nombre: str = nombre
+        self.id_usuario: str = id_usuario
+        self.libros_prestados: List[str] = []  # Lista de ISBNs
+
+    def __str__(self) -> str:
+        return f"Usuario {self.nombre} (ID: {self.id_usuario}), prestados: {len(self.libros_prestados)}"
