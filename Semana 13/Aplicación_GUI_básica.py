c5d84786659afcc393c903fe25201ae6f0ba716f
kerly suarez <kerly_suarezb@hotmail.com>
2026-03-12 09:41:05 -0500

    Descripción de la aplicación

---
+++ Semana 13/Aplicación_GUI_básica.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+"""
+Descripción de la Aplicación
+
+Esta aplicación es un Registro de Contactos desarrollado con Python y Tkinter 
+que permite a los usuarios almacenar y visualizar información de contactos de 
+forma intuitiva. 
+
+La interfaz incluye campos de texto para ingresar nombre, número de teléfono y 
+correo electrónico, un botón "Agregar" para insertar los datos en una tabla, y 
+un botón "Limpiar" que borra toda la información ingresada y la tabla. Utiliza 
+ttk.Treeview para mostrar los contactos en formato tabular con columnas 
+organizadas, respondiendo a los eventos de clic del usuario de manera eficiente.
+"""
+
+
